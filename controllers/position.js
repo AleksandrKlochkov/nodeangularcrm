4ebf50f4e8ffc9c91812caebf799b70ea0209953
Alex <surfer.ru@mail.ru>
2019-08-23 20:44:02 +0400

    addings files projects

---
+++ controllers/position.js
@@ -0,0 +1,18 @@
+const Position = require('../models/Position')
+const errorHandler = require('../utils/errorHandler')
+
+module.exports.getByCategoryId = function(req, res) {
+
+}
+
+module.exports.create = function(req, res) {
+
+}
+
+module.exports.remove = function(req, res) {
+
+}
+
+module.exports.update = function(req, res) {
+
+}
